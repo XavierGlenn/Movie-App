20f5963b89b7119221b56a9a5628a16eeb36ab1b
Xavier Glenn <xglenn@appdev.yearup.org>
2024-09-24 13:54:17 -0700

    Movie App Project
    Movie App Project completed on 9/23/24 and 9/24/24

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main ( String[] args ) {
+        System.out.println("Hello user!");
+    }
+}
+
+{
+    String movieTitle = "Movie";
+String releaseYear = "2000";
+String movieRating = "PG-13";
+boolean favorite = 'true';
+int reviewnumbers = 30;
+String directorName = "Some Guy";
+
+String username = "test";
+String email = "testemail@email.com";
+String password = "testpassword";
+String ccNumber = "11111111111111";
+String birthday = "01/01/2000";
+String address = "123 Main Street, Somewhere USA 11111";
+
+System.out.println("Welcome to The Movie App! Sign up below!" + username);
+System.out.println("Enter your username: " + username);
+System.out.println("Enter your password: " + password);
+System.out.println("Enter your credit card details: " + ccNumber);
+System.out.println("Enter your billing address: " + address);
+System.out.println("Enter your birthday: " + birthday);
+}
